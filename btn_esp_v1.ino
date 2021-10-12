b8118cee7cdcb958af4704dd0dda35159199b11d
Roman Maksymiv <r.maks@hotmail.com>
2021-10-12 11:11:18 +0300

    http or https

--- btn_esp_v1.ino
+++ btn_esp_v1.ino
@@ -8,10 +8,6 @@
 //const char* ssid     = "[MikroTik]";
 //const char* password = "19591983";
 
-
-
-const char* PARAM_INPUT_1 = "state";
-
 const int output = 4;  //5
 const int buttonPin = 5;  //2
 
@@ -29,10 +25,9 @@ unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 AsyncWebServer server(80);
-
 const char* PARAM_INPUT = "inputdata";
 
-// HTML web page to handle 3 input fields (input1, input2, input3)
+// HTML
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
 <html lang="en">
@@ -78,8 +73,6 @@ const char index_html[] PROGMEM = R"rawliteral(
     </div>
 </body>
 
-
-
 </html>
 
 <script>
@@ -100,61 +93,60 @@ const char index_html[] PROGMEM = R"rawliteral(
 void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
 }
-
-
-
       
 void setup(){
+//    uint8_t newMACAddress[] = {0x30, 0xAE, 0xA4, 0x07, 0x0D, 0x66}; 
+//  String newHostname = "button01";
+//
+//  wifi_set_macaddr(STATION_IF, &newMACAddress[0]);
+//   WiFi.hostname(newHostname.c_str());
+//  WiFi.mode(WIFI_STA); 
+  pinMode(output, OUTPUT);
+    digitalWrite(output, LOW);
+  pinMode(buttonPin, INPUT);
+
   delay(2000);
-  Serial.begin(115200);
-   
+  Serial.begin(115200);   
   EEPROM.begin(1024);
  
-  Serial.println("Reading EEPROM ssid");
- 
+  Serial.println("Reading EEPROM ssid"); 
   String esid;
-  for (int i = 0; i < 32; ++i)
-  {
-    esid += char(EEPROM.read(i));
-  }
+    for (int i = 0; i < 32; ++i)
+    {
+      esid += char(EEPROM.read(i));
+    }
   Serial.println();
   Serial.print("SSID: ");
   Serial.println(esid);
   Serial.println("Reading EEPROM pass");
  
   String epass = "";
-  for (int i = 32; i < 96; ++i)
-  {
-    epass += char(EEPROM.read(i));
-  }
+    for (int i = 32; i < 96; ++i)
+    {
+      epass += char(EEPROM.read(i));
+    }
   Serial.print("PASS: ");
   Serial.println(epass);
 
   String ehost = "";
-  for (int i = 96; i < 224; ++i)
-  {
-    ehost += char(EEPROM.read(i));
-  }
+    for (int i = 96; i < 224; ++i)
+    {
+      ehost += char(EEPROM.read(i));
+    }
   Serial.print("HOST: ");
   Serial.println(ehost);
 
   String eevent = "";
-  for (int i = 224; i < 512; ++i)
-  {
-    eevent += char(EEPROM.read(i));
-  }
+    for (int i = 224; i < 512; ++i)
+    {
+      eevent += char(EEPROM.read(i));
+    }
   Serial.print("EVENT: ");
   Serial.println(eevent);
 
-
-       qeventhost = ehost;
-       qevents = eevent;
+    qeventhost = ehost;
+    qevents = eevent;
        
-  /////////////////////
-  pinMode(output, OUTPUT);
-  digitalWrite(output, LOW);
-  pinMode(buttonPin, INPUT);
-
   //==========if btn is presed then wifi hotspot is active untill reboot
   
     int reading = digitalRead(buttonPin);
@@ -170,92 +162,63 @@ void setup(){
           WiFi.softAP("BUTTON_CONFIG", "1133557799");
             IPAddress IP = WiFi.softAPIP();
             Serial.print("AP IP address: ");
-            Serial.println(IP);
-//          while (WiFi.status() != WL_CONNECTED) {
-//            delay(500);
-//            Serial.print(".");
-//        }
-          
-  }
-    
-    else{ 
- 
-  // Connect to Wi-Fi
-
-String newHostname = "button01";
-WiFi.mode(WIFI_STA);
-
-WiFi.hostname(newHostname.c_str());
-
-Serial.printf("New hostname: %s\n", WiFi.hostname().c_str());
-  
-  WiFi.begin(esid, epass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-    digitalWrite(output,!digitalRead(output));
-  }
- digitalWrite(output, HIGH); 
- }
- //==========
- 
+            Serial.println(IP);          
+        }    
+    else{  
+          // Connect to Wi-Fi
+          Serial.printf("hostname: %s\n", WiFi.hostname().c_str());
+            WiFi.begin(esid, epass);
+              while (WiFi.status() != WL_CONNECTED) {
+                delay(1000);
+                Serial.println("Connecting to WiFi..");
+                digitalWrite(output,!digitalRead(output));
+              }
+          digitalWrite(output, HIGH); 
+        } 
   // Print ESP Local IP Address
-  Serial.println(WiFi.localIP());
-  
+ Serial.println(WiFi.localIP());  
   // Route for root / web page
  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html);
   });
 
-  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
+  // Send a GET request to <host_IP>/get?
   server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-
     String inputMessage;
     String inputParam;
-    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
-    if (request->hasParam(PARAM_INPUT)) {
-      inputMessage = request->getParam(PARAM_INPUT)->value();
-      inputParam = PARAM_INPUT;}
-
-      
-    else {
-      inputMessage = "No message sent";
-      inputParam = "none";
-    }
+    // GET input value on <ESP_IP>/get?input
+      if (request->hasParam(PARAM_INPUT)) {
+        inputMessage = request->getParam(PARAM_INPUT)->value();
+        inputParam = PARAM_INPUT;}        
+      else {
+        inputMessage = "No message sent";
+        inputParam = "none";
+      }
     
-    ////parce JSON
-      StaticJsonDocument<600> doc;
+////parce JSON  
 //    String stri ;
-//
-//// Length (with one extra character for the null terminator)
 //int str_len = inputMessage.length() + 1; 
-//
-//// Prepare the character array (the buffer) 
 //char char_array[str_len];
-//
-//// Copy it over 
 //stri.toCharArray(char_array, str_len);
 
-
+  StaticJsonDocument<600> doc;
   deserializeJson(doc, inputMessage);
-
-  //char json[] =inputMessage;
   JsonObject data = doc["inputdata"];
-  Serial.println(data);
+    Serial.println(data);
   const char* wifiname = data["wifiname"];
   const char* wifipass = data["wifipass"];
   const char* eventhost = data["eventhost"];
   const char* events = data["events"];
-Serial.println(events);
+    Serial.println(events);
+
+    Serial.println("jjjjjson large: ");
+    Serial.println(data.size());
   /////////////////EEPROM write////////////////////////////////
-//      String qsid = wifiname;
-//      String qpass = wifipass;
-//      String qeventhost = eventhost;
-//      String qevents = events;
-       qsid = wifiname;
-       qpass = wifipass;
-       qeventhost = eventhost;
-       qevents = events;
+    qsid = wifiname;
+    qpass = wifipass;
+    qeventhost = eventhost;
+    qevents = events;
+
       if (qsid.length() > 0 && qpass.length() > 0) {
         Serial.println("clearing eeprom");
         for (int i = 0; i < 1024; ++i) {
@@ -269,7 +232,7 @@ Serial.println(events);
                 Serial.println("");
                 Serial.println(qevents);
                 Serial.println("");
-                
+
         Serial.println("writing eeprom ssid:");
         for (int i = 0; i < qsid.length(); ++i)
         {
@@ -335,26 +298,44 @@ if (reading ==0) {
           
                //
 
+    
                   char Buf[128];
                   qeventhost.toCharArray(Buf, 128);
                   char evBuf[128];
                   qevents.toCharArray(evBuf, 128);
+
+    String ifsecure = qeventhost.substring(4,5);
+    if (ifsecure=="s"){
+        Serial.println("secure"+ifsecure);
+          std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+          client->setInsecure();                 
+          HTTPClient http;
+          Serial.println(qevents + "--HOST-:" + Buf);                  
+          http.begin(*client, Buf);           //////qeventhost or "https://webhook.site/71c2f7ef-1dff-492b-9377-5757859cb3c4"
+          http.addHeader("Content-Type", "application/json");                  
+          int httpCode = http.POST(qevents);    ///////// "{\"eventName\":\"DYNAMIC_EVENT\"}"             
+          Serial.println(httpCode);                    
+          http.end(); 
+    }
+    else{
+        Serial.println("not secure"+ifsecure);
+          WiFiClient client;
+          HTTPClient http;                    
+          Serial.println(qevents + "--HOST-:" + Buf);                  
+          http.begin(client, Buf);         //http://192.168.2.71:55554/event
+          http.addHeader("Content-Type", "application/json");                  
+          int httpCode = http.POST(evBuf);                 
+          Serial.println(httpCode);                    
+          http.end(); 
+    }
+
+    digitalWrite(output, HIGH);
+    delay(3000);
+    digitalWrite(output, LOW);
     
     
-//                  std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
-//                  client->setInsecure();
-//                  
-//                    //WiFiClient client;
-//                    HTTPClient http;
-//                      Serial.println(qevents + "--HOST-:" + Buf);                  
-//                    http.begin(*client, Buf);           //////qeventhost or "https://webhook.site/71c2f7ef-1dff-492b-9377-5757859cb3c4"
-//                    http.addHeader("Content-Type", "application/json");                  
-//                    int httpCode = http.POST(qevents);    ///////// "{\"eventName\":\"DYNAMIC_EVENT\"}"             
-//                    Serial.println(httpCode);                    
-//                    http.end();  
-//                      digitalWrite(output, HIGH);
-//                      delay(3000);
-//                      digitalWrite(output, LOW);
+ 
+
 //
 //            //
 
@@ -362,39 +343,10 @@ if (reading ==0) {
                   //std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
                   //client->setInsecure();
                   
-                    WiFiClient client;
-                    HTTPClient http;
-                    
-                    Serial.println(qevents + "--HOST-:" + Buf);                  
-                    http.begin(client, Buf); //http://192.168.2.71:55554/event
-                    http.addHeader("Content-Type", "application/json");                  
-                    int httpCode = http.POST(evBuf);                 
-                    Serial.println(httpCode);                    
-                    http.end();  
+ 
             //
 
   }
 
-  // If the switch changed, due to noise or pressing:
-  if (reading != lastButtonState) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
-
-    // if the button state has changed:
-    if (reading != buttonState) {
-      buttonState = reading;
-
-      // only toggle the LED if the new button state is HIGH
-      if (buttonState == HIGH) {
-        ledState = !ledState;
-      }
-    }
-  }
-
-  lastButtonState = reading;
 }
