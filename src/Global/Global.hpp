5fa87f59137e9b7da5e8535996d06b0d47b8abcf
r.maks <roman.maksymiv@novisign.de>
2023-12-08 15:08:25 +0200

    #45 - Events null check

--- src/Global/Global.hpp
+++ src/Global/Global.hpp
@@ -11,7 +11,7 @@
 
 extern Logger logger;
 
-const String currentFirmwareVersion = String("1.2.0 - build date: ") + __DATE__;
+const String currentFirmwareVersion = String("1.2.1 - build date: ") + __DATE__;
 
 struct WiFiCONFIG {
     String ssid;
