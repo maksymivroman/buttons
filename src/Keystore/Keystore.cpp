192cfcbe2a0f52665f0fba6c8bc7f921b3231cd3
r.maks <roman.maksymiv@novisign.de>
2023-12-27 11:59:32 +0200

    #48 - Keystore class

---
+++ src/Keystore/Keystore.cpp
@@ -0,0 +1,46 @@
+//
+// Created by rmaks on 25-Dec-23.
+//
+
+#include "Keystore.h"
+
+
+Keystore::Keystore(unsigned int maxKeystoreItemsToStore) {
+    this->maxKeystoreItems = maxKeystoreItemsToStore;
+}
+
+void Keystore::clearKeystore() {
+    const unsigned int totalCount = this->items.size();
+    this->items.clear();
+    logger.log("[Keystore]: Clear... ", "removed ", totalCount, " items");
+}
+
+void Keystore::addItem(String key, String value) {
+    //TODO max items
+    auto addResult = this->items.insert({key, value});
+
+    if (!addResult.second) {
+        this->items[key] = value;
+        logger.logSerial("[Keystore]: Update item - ", key);
+    }
+}
+
+bool Keystore::removeItem(String &key) {
+    auto search = this->items.find(key);
+    if (search != this->items.end()) {
+        this->items.extract(key);
+        logger.log("[Keystore]: Remove item - ", key, ". Success");
+        return true;
+    }
+    logger.log("[Keystore]: Remove item - ", key, ". Item not exist!");
+    return false;
+}
+
+String Keystore::itemValue(String &key) {
+    auto search = this->items.find(key);
+    if (search != this->items.end()) {
+        return search->second;
+    }
+    return this->emptyReturnValue;
+}
+
---
+++ src/Keystore/Keystore.h
@@ -0,0 +1,32 @@
+//
+// Created by rmaks on 25-Dec-23.
+//
+
+#ifndef EVENT_BUTTON_KEYSTORE_H
+#define EVENT_BUTTON_KEYSTORE_H
+
+#include <Arduino.h>
+#include <map>
+#include "Global/Global.hpp"
+
+class Keystore {
+
+public:
+    explicit Keystore(unsigned int maxKeystoreItemsToStore);
+
+    void clearKeystore();
+    void addItem(String key, String value);
+
+    bool removeItem(String &key);
+    String itemValue(String &key);
+
+private:
+    std::map<String, String> items;
+
+    const String emptyReturnValue = "";
+    unsigned int maxKeystoreItems;
+
+};
+
+
+#endif //EVENT_BUTTON_KEYSTORE_H
